4b7654613a3ca01b49628fd96d7f076506d36b35
Pedro Verner <53663456+PedroW21@users.noreply.github.com>
2022-01-27 17:05:31 -0300

    FIm do curso

---
+++ Primeiros passos para o desenvolvimento web/index.php
@@ -0,0 +1,14 @@
+<html>
+
+<title>Php Da Depression</title>
+
+<body>
+    <?php
+    for( $i = 0; $i <10; $i++) {
+        print("Linha numero" . $i . "<br />");
+    }
+    ?>
+</body>
+
+
+</html>
