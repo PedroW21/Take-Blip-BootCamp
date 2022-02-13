d34422e0223cfc473eacad7d7536ab92fd646a42
Pedro Verner <53663456+PedroW21@users.noreply.github.com>
2022-02-13 17:51:05 -0300

    Fim do modulo 2

---
+++ Introdução ao JavaScript/Recursos Básicos/funções.js
@@ -0,0 +1,15 @@
+// Metódo normal
+function soma(a, b) {
+  return a+b;
+}
+
+console.log(soma(5,5));
+
+
+// Arrow
+
+soma = (a,b) => {
+    return a+b;
+}
+
+console.log(soma(4,4));
---
+++ Introdução ao JavaScript/Recursos Básicos/manipulando.js
@@ -0,0 +1,5 @@
+// Comentário de uma linha
+
+var hello = "Hello World!";
+
+console.log(hello); /*Comentário para multiplas linhas */
