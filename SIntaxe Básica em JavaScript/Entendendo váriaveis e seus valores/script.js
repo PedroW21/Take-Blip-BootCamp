08e026178969d2af9d7f5d7538cece1282c8064f
Pedro Verner <53663456+PedroW21@users.noreply.github.com>
2022-02-19 09:23:18 -0300

    Fim do modulo

---
+++ SIntaxe Básica em JavaScript/Entendendo váriaveis e seus valores/script.js
@@ -0,0 +1,34 @@
+// booleanos
+// True or False
+
+var temBanana = false;
+
+console.log(typeof(temBanana)); // typeof() mostra o tipo da variável (number, boolean, function e etc...)
+
+// number
+
+var numero = 2;
+
+console.log(typeof(numero)); 
+
+// string
+
+var pessoa = "Maria";
+
+console.log(typeof(pessoa));
+
+// Var = global / Let = de bloco / Const = Bloco, somente leitura
+
+// Operadores aritmédicos
+
+console.log(1+1); //soma
+
+console.log(3-1); //subtração
+
+console.log(4/2) //divisao
+
+console.log(4%2) // Resto da divisão
+
+console.log(2*1) // divisão
+
+console.log(2**2) // potenciação
