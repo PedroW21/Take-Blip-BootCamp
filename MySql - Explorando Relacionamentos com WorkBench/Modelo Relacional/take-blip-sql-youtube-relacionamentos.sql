31d6612459f85db73ae7cde249705a6fa6c23a56
Pedro Verner <53663456+PedroW21@users.noreply.github.com>
2022-03-16 11:07:03 -0300

    Organizando o arquivo youtube e realizando consulta
    Consulta realizada buscando o titulo do video, seu autor e sua tag no SEO

--- MySql - Explorando Relacionamentos com WorkBench/Modelo Relacional/take-blip-sql-youtube-relacionamentos.sql
+++ MySql - Explorando Relacionamentos com WorkBench/Modelo Relacional/take-blip-sql-youtube-relacionamentos.sql
@@ -1,3 +1,19 @@
 USE youtube;
 
 SELECT * FROM videos;
+
+-- solicitei todos os dados da tabela videos e juntei com autor, sendo a FK de videos igual a id_autor da tabela autor;
+SELECT * FROM videos JOIN autor ON videos.FK_autor = autor.id_autor; -- consulta generalizada (aparece tudo);
+
+-- Aqui solicitei somente a coluna titulo dos videos e quem o fez;
+SELECT videos.titulo, autor.nome FROM videos JOIN autor ON videos.FK_autor = autor.id_autor; 
+
+-- solicitei a junção das tabelas videos e seo e defini que a FK_SEO é o id_seo da tabela SEO (e pedi que mostrasse tudo);
+SELECT * FROM videos JOIN SEO ON videos.FK_SEO = SEO.id_seo; 
+
+-- Aqui restringi a solicitação para mostrar somente o titulo do video e sua categoria;
+SELECT videos.titulo, SEO.categorias FROM videos JOIN SEO ON videos.FK_SEO = SEO.id_seo;
+
+-- Estou adicionando a tabela autor na relação, e dizendo que o respectivo video foi feito por tal autor e tem tal tag de SEO (será mostrado isso);
+SELECT videos.titulo, autor.nome, SEO.categorias FROM videos JOIN SEO ON videos.FK_SEO = SEO.id_seo 
+JOIN autor ON videos.FK_autor = autor.id_autor;
--- MySql - Explorando Relacionamentos com WorkBench/Modelo Relacional/take-blip-sql-youtubedb.sql
+++ MySql - Explorando Relacionamentos com WorkBench/Modelo Relacional/take-blip-sql-youtubedb.sql
@@ -1,7 +1,6 @@
 CREATE DATABASE youtube;
 
 USE youtube;
-DROP TABLE videos;
 
 CREATE TABLE autor (
 id_autor INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
@@ -9,11 +8,14 @@ nome VARCHAR(20),
 data_nasc DATE
 );
 
+/*** Alterações na tabela Autor ***/
+
 INSERT INTO autor(nome, data_nasc) VALUES ("Pierre", "2002-07-25");
 INSERT INTO autor(nome, data_nasc) VALUES ("Hugo", "2002-10-30");
 INSERT INTO autor(nome, data_nasc) VALUES ("Anne", "2002-12-23");
 INSERT INTO autor(nome, data_nasc) VALUES ("Edmar", "1963-01-23");
 
+/*** FIM DAS ALTERAÇÕES ***/
 
 CREATE TABLE videos(
 id_video INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
@@ -24,6 +26,20 @@ titulo VARCHAR(50) NOT NULL,
 likes INT NOT NULL,
 deslikes INT NOT NULL);
 
+/***Alterações na tabela videos ***/
+INSERT INTO videos (FK_autor, titulo, likes, deslikes) VALUES (1, "O começo de uma jornada - EP01", 21785, 2378);
+INSERT INTO videos (FK_autor, titulo, likes, deslikes) VALUES (2, "SpeedRacer - Destrinchando sua história", 12659, 213);
+INSERT INTO videos (FK_autor, titulo, likes, deslikes) VALUES (3, "A filosofia libertária", 154698, 3977);
+
+ALTER TABLE videos ADD FK_SEO INT NOT NULL AFTER titulo; -- adicionado depois na tabela
+ALTER TABLE videos ADD FOREIGN KEY(FK_SEO) REFERENCES SEO(id_seo); -- definindo FK_SEO como uma FOREIGN KEY
+
+UPDATE videos SET FK_SEO=1 WHERE id_video=1;
+UPDATE videos SET FK_SEO=1 WHERE id_video=2;
+UPDATE videos SET FK_SEO=2 WHERE id_video=3;
+
+/*** FIM DAS ALTERAÇÕES ***/
+
 CREATE TABLE playlist (
 
 id_playlists INT NOT NULL PRIMARY KEY,
@@ -31,11 +47,9 @@ videos INT NOT NULL,
 title VARCHAR(50) NOT NULL);
 
 create table SEO (
-id_seo INT NOT NULL PRIMARY KEY,
+id_seo INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 categorias VARCHAR(12) NOT NULL
 );
 
-
-INSERT INTO videos (FK_autor, titulo, likes, deslikes) VALUES (1, "O começo de uma jornada - EP01", 21785, 2378);
-INSERT INTO videos (FK_autor, titulo, likes, deslikes) VALUES (2, "SpeedRacer - Destrinchando sua história", 12659, 213);
-INSERT INTO videos (FK_autor, titulo, likes, deslikes) VALUES (3, "A filosofia libertária", 154698, 3977);
+INSERT INTO SEO(categorias) VALUES ("Cinema");
+INSERT INTO SEO(categorias) VALUES ("Filosofia");
